0032e3d5b972f2c4ae324332a437c5117790b512
Claudio Pinto <me@cfpinto.com>
2021-03-23 00:03:30 +0000

    Squashed commit of the following:
    commit 777d77b5e40bc8f65b07e3393545e0b5148e4bde
    Author: Claudio Pinto <me@cfpinto.com>
    Date:   Tue Mar 23 00:03:09 2021 +0000
    
        added user setting

--- src/Console/SupervisorConf.php
+++ src/Console/SupervisorConf.php
@@ -12,7 +12,8 @@ class SupervisorConf extends Command
 
     protected $signature = 'make:supervisor 
         {name : The name of the queue supervisor is monitoring} 
-        {--num_proc : How many instances of the queue worker}
+        {--num_proc= : How many instances of the queue worker. Default 2}
+        {--user= : User running the command. Default ubuntu}
         {--write : ask to write to destination. needs sudo}';
 
     protected $description = 'Create a supervisor conf file for the necessary workers';
@@ -26,14 +27,15 @@ class SupervisorConf extends Command
     {
         $name = $this->argument('name');
         $numProcesses = $this->option('num_proc') ?? 2;
+        $user = $this->option('user') ?? 'ubuntu';
         $write = $this->option('write');
         $artisan = $this->getLaravel()->basePath() . '/artisan';
         $path = realpath(__DIR__ . '/../../assets/supervisor/conf.d/conf.stub');
         $stub = file_get_contents($path);
         $logPath = storage_path('logs/' . $name . '.worker.log');
         $content = str_replace(
-            ['%(queue_name)', '%(artisan_path)', '%(log_file_path)', '%(number_processes)'],
-            [$name, $artisan, $logPath, $numProcesses],
+            ['%(queue_name)', '%(artisan_path)', '%(log_file_path)', '%(number_processes)', '%(user)'],
+            [$name, $artisan, $logPath, $numProcesses, $user],
             $stub
         );
 
